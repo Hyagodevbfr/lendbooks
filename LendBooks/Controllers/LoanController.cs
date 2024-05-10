bdc19ed8b77089b4e511aefece0d321f90d1ff62
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-10 13:53:42 -0300

    Continue Configuration LoanPage

--- LendBooks/Controllers/LoanController.cs
+++ LendBooks/Controllers/LoanController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LendBooks.Infra.Data;
+using LendBooks.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LendBooks.Controllers;
 public class LoanController: Controller
 {
+    readonly private ApplicationDbContext _dbContext;
+    public LoanController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
     public IActionResult Index()
     {
-        return View( );
+        IEnumerable<LoansModel> loans = _dbContext.Loans!;
+        return View(loans);
     }
 }
