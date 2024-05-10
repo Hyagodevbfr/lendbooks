c1e551d960e6ebc1c52f5c9ecadc1fab02e4d038
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-10 10:47:43 -0300

    Create LoansModel Model

---
+++ LendBooks/Models/LoansModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LendBooks.Models;
+
+public class LoansModel
+{
+    public Guid Id { get; set; }
+    public string Reciver { get; set; } = null!;
+    public string Supplier { get; set; } = null!;
+    public string BorrowedBook { get; set; } = null!;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+}
