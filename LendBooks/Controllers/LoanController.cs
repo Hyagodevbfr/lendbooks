cff20ea3d0ecd9bb6cd00d964fb58a8f02055ca8
hyagodevbfr <hyago.sbrito@gmail.com>
2024-05-10 13:27:05 -0300

    create initial loan configuration

---
+++ LendBooks/Controllers/LoanController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LendBooks.Controllers;
+public class LoanController: Controller
+{
+    public IActionResult Index()
+    {
+        return View( );
+    }
+}
